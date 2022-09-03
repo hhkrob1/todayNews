ac43afa9de57d17c30afb46080d515d8add01b19
lufei <lufei@126.com>
2022-09-03 09:54:07 +0800

    2022_09_03_095405

--- everyDevelop.py
+++ everyDevelop.py
@@ -27,17 +27,27 @@ def get_every_dev(news_url, moyu_url, calendarData):
     soup = BeautifulSoup(news_resp, "html.parser")
     # resl = ['<a href="{}">{}</a>'.format(news_url + i.attrs.get(
     # "href"), i.text) for i in soup.select('.daily>.posts .post .title a')]
-    resl = ['{}</a>'.format(news_url + i.attrs.get(
-    "href"), i.text) for i in soup.select('.daily>.posts .post .title a')]
+    resl = {news_url + i.attrs.get(
+    "href"):i.text for i in soup.select('.daily>.posts .post .title a')}
     res = ""
-    for i in range(len(resl)):
-        dot_str = "." if (i + 1) >=10 else ".   "
-        # res += "<b>" + "{: <4s}".format(str(i + 1)+dot_str) + resl[i] + "\n" + "</b>"
-        res += "<b>" + '<a href="{}">' + "{: <4s}".format(str(i + 1)+dot_str) + resl[i] + "\n" + "</b>"
+    nnum = 0
+
+    # for i in range(len(resl)):
+    #     dot_str = "." if (i + 1) >=10 else ".   "
+    #     res += "<b>" + "{: <4s}".format(str(i + 1)+dot_str) + resl[i] + "\n" + "</b>"
+
+    for i, k in resl.items():
+        dot_str = "." if (nnum + 1) >=10 else ".   "
+        num = "{: <4s}".format(str(nnum + 1)+dot_str)
+        acontent = '<a href="{}">{}</a>'.format(i,num + k)
+        res += "<b>" + acontent + "\n" + "</b>"
+        nnum += 1
 
     # Return data
     return moyu_res + res
 
 
 if __name__ == '__main__':
+    res = get_every_dev("https://toutiao.io","https://api.j4u.ink/v1/store/other/proxy/remote/moyu.json","123")
+    print(res)
     pass
